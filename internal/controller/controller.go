21ada25665f1339cefd44c1fd8e5d722a3f3e2aa
Aleksey Solovyev <solovyev.a@icloud.com>
2023-02-01 13:22:11 +0300

    feat: add Logger and Recoverer middlewares

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -3,10 +3,14 @@ package controller
 import (
 	"github.com/alsolovyev/dummy-api/internal/entity"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func New(f entity.FileUseCaser) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Get("/ping", handlePing)
